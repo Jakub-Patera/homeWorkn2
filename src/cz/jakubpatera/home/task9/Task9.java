22445227ba85deda81106f8661a2c461093da1ef
dereck686 <patera.jakub686@gmail.com>
2021-01-16 22:38:19 +0100

    ukol 9

--- src/cz/jakubpatera/home/task9/Task9.java
+++ src/cz/jakubpatera/home/task9/Task9.java
@@ -1,2 +1,18 @@
-package cz.jakubpatera.home.task9;public class Task9 {
+package cz.jakubpatera.home.task9;
+
+import java.util.Scanner;
+
+import static java.lang.StrictMath.floor;
+
+public class Task9 {
+    public static void main(String[] args) {
+        System.out.println("Check if number have real part:");
+        Scanner sc = new Scanner(System.in);
+        double a = Double.parseDouble(sc.nextLine());
+        if( floor(a) == a ){
+            System.out.println("number " + a + " has not real part");
+        }else {
+            System.out.println("number " + a + "has real part");
+        }
+    }
 }
